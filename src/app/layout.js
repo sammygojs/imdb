0620b63453acb7683029077b75ac3c1ce127ee9b
Sumit <akoliyasumit@hotmail.com>
2024-01-28 14:07:50 +0000

    added dark mode

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
+// "user client";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
+import Providers from "./Providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,6 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Providers>
         {/* <Headers/> */}
         <Header/>
         
@@ -21,7 +24,7 @@ export default function RootLayout({ children }) {
         {/* <SearchBox/> */}
         
         {children}
-        
+        </Providers>
         </body>
     </html>
   );
