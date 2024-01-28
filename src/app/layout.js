2757b09e65c3eda77c044fb612da9c020c4beb40
Sumit <akoliyasumit@hotmail.com>
2024-01-28 14:26:56 +0000

    added About section

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
 import Providers from "./Providers";
+import About from "./about/About";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({ children }) {
         {/* <Headers/> */}
         <Header/>
         
+        <About />
         {/* <Navbar/> */}
 
         {/* <SearchBox/> */}
